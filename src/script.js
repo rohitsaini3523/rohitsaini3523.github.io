40d95e12878b9a168e830171bf5f4bd13d94a5f1
rohitsaini3523 <rohitsaini3523@gmail.com>
2023-07-13 09:52:32 +0530

    react basic

--- src/script.js
+++ src/script.js
@@ -1,12 +1,13 @@
-const quote = document.getElementById("value");
-const author = document.getElementById("author-name");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author_name");
 function random() {
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
-      // console.log(result);
       quote.innerText = result.content;
       author.innerText = result.author;
     });
 }
 random();
+
+
