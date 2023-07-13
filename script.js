f1325f26712f5f4123e5d764360ddd299e4532fc
rohitsaini3523 <rohitsaini3523@gmail.com>
2023-07-13 10:01:42 +0530

    Revert "react basic"
    This reverts commit 40d95e12878b9a168e830171bf5f4bd13d94a5f1.

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const quote = document.getElementById("quote");
-const author = document.getElementById("author_name");
+const quote = document.getElementById("value");
+const author = document.getElementById("author-name");
 function random() {
   fetch("https://api.quotable.io/random")
     .then((res) => res.json())
     .then((result) => {
+      // console.log(result);
       quote.innerText = result.content;
       author.innerText = result.author;
     });
 }
 random();
-
-
