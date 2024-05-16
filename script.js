39191680a42dbea62d6f3a0473f89c8c755818f6
Rohit Saini <rohitsaini3523@gmail.com>
2024-05-16 17:10:00 +0530

    Updated Portfolio

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
+
 const quote = document.getElementById("value");
 const author = document.getElementById("author-name");
-function random() {
-  fetch("https://api.quotable.io/random")
-    .then((res) => res.json())
-    .then((result) => {
-      // console.log(result);
-      quote.innerText = result.content;
-      author.innerText = result.author;
-    });
+
+async function random() {
+    fetch("https://api.quotable.io/random")
+        .then((res) => res.json())
+        .then((result) => {
+            // console.log(result);
+            quote.innerText = result.content;
+            author.innerText = result.author;
+        });
 }
 random();
